3181cbbdd434d200dd255b291287e84ef2d43d95
Matth <187baseops@gmail.com>
2025-08-26 14:13:18 -0500

    Ensure /kiosk route points to KioskDashboard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import KioskDashboard from "./KioskDashboard";
 
 export default function App() {
   return (
-    <Router basename="/">
+    <BrowserRouter>
       <Routes>
-        {/* Explicit kiosk route */}
-        <Route path="/kiosk" element={<KioskDashboard />} />
-
-        {/* Main dashboard */}
         <Route path="/" element={<Dashboard />} />
-
-        {/* Catch-all fallback */}
-        <Route path="*" element={<Dashboard />} />
+        <Route path="/kiosk" element={<KioskDashboard />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
